14e29155b181469e8caa88a6350a0c16c4bb8fa8
hetvii28 <hetvitshah28@gmail.com>
2025-07-31 17:12:34 +0530

    Update battery_dashboard.py

--- battery_dashboard.py
+++ battery_dashboard.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
-import plotly.express as px
-from plotly.subplots import make_subplots
 import random
 import time
 from datetime import datetime
